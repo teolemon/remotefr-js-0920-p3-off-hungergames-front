511622755a6d6e56c03948fef7eb4b81ad7152a2
lgiacalo <lgiacalo@student.42.fr>
2021-01-02 20:27:44 +0100

    add request questions by ROBOTOFF_API_URL

--- src/const.js
+++ src/const.js
@@ -4,3 +4,6 @@ export const ROBOTOFF_API_URL = "https://robotoff.wild31.com/api/v1";
 export const OFF_URL = "https://world.openfoodfacts.org";
 export const OFF_API_URL = `${OFF_URL}/api/v0`;
 export const OFF_IMAGE_URL = "https://static.openfoodfacts.org/images/products";
+
+
+export const BACK_API_NODE = 'http://localhost:5000/robotoff';
--- src/robotoff.js
+++ src/robotoff.js
@@ -1,53 +1,54 @@
-import axios from 'axios';
-import { ROBOTOFF_API_URL } from "./const"
+import axios from "axios";
+import { ROBOTOFF_API_URL, BACK_API_NODE } from "./const";
 import { getLang } from "./settings";
-import { removeEmptyKeys } from './utils'
+import { removeEmptyKeys } from "./utils";
 
 export default {
-
   annotate(insightId, annotation) {
     return axios.post(
       `${ROBOTOFF_API_URL}/insights/annotate`,
       new URLSearchParams(
-        `insight_id=${insightId}&annotation=${annotation}&update=1`,
+        `insight_id=${insightId}&annotation=${annotation}&update=1`
       ),
-      { withCredentials: true },
-    )
+      { withCredentials: true }
+    );
   },
 
   questions(sortBy, insightTypes, valueTag, brands, country, count = 10) {
     const lang = getLang();
-    return axios.get(
-      `${ROBOTOFF_API_URL}/questions/${sortBy}`, {
-      params: removeEmptyKeys({
-        count, lang, insight_types: insightTypes, value_tag: valueTag, brands, country
+    return axios
+      .get(`${BACK_API_NODE}/questions`, {
+        params: removeEmptyKeys({
+          count,
+          lang,
+          insight_types: insightTypes,
+          value_tag: valueTag,
+          brands,
+          country,
+        }),
       })
-    }
-    ).then(result => {
-      let questions = result.data.questions;
-      result.data.questions = questions.filter(question => question.source_image_url);
-      return result;
-    })
+      .then((result) => result);
   },
 
   getInsights(barcode, insightTypes, valueTag, annotation, page, count = 25) {
-    let annotated
+    let annotated;
     if (annotation.length && annotation == "not_annotated") {
       annotated = "0";
     }
-    return axios.get(
-      `${ROBOTOFF_API_URL}/insights`, {
+    return axios.get(`${ROBOTOFF_API_URL}/insights`, {
       params: removeEmptyKeys({
-        barcode, insight_types: insightTypes,
-        value_tag: valueTag, annotation, page,
-        annotated, count
-      })
-    }
-    )
+        barcode,
+        insight_types: insightTypes,
+        value_tag: valueTag,
+        annotation,
+        page,
+        annotated,
+        count,
+      }),
+    });
   },
 
   getUserStatistics(username) {
-    return axios.get(`${ROBOTOFF_API_URL}/users/statistics/${username}`)
+    return axios.get(`${ROBOTOFF_API_URL}/users/statistics/${username}`);
   },
-
-}
+};
